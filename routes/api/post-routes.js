ff1c380b517726d8bb19cd4f4bb31ac766354891
Elena Carey <theelenacarey@gmail.com>
2022-07-20 23:41:23 -0500

    Added Comments. Modified User and Post to include comments

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User, Vote } = require('../../models');
-const { create } = require('../../models/User');
+const { Post, User, Vote, Comment } = require('../../models');
+
 
 //get all users
 router.get('/', (req, res) => {
@@ -17,6 +17,20 @@ router.get('/', (req, res) => {
         ],
         order: [['created_at', 'DESC']],
         include: [
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_text',
+                    'post_id',
+                    'user_id',
+                    'created_at'
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
@@ -43,6 +57,20 @@ router.get('/:id', (req, res) => {
             [sequelize.literal('(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
         include: [
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_text',
+                    'post_id',
+                    'user_id',
+                    'created_at'
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes:  ['username']
