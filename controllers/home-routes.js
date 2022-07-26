92473657884def78f11e7d447e09712977eb1056
Elena Carey <theelenacarey@gmail.com>
2022-07-26 16:32:29 -0500

    Added log in and log out functionality.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,7 @@ const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 
 router.get("/", (req, res) => {
+	console.log(req.session);
 	Post.findAll({
 		attributes: [
 			"id",
@@ -32,7 +33,7 @@ router.get("/", (req, res) => {
 		],
 	})
 		.then((dbPostData) => {
-            const posts = dbPostData.map(post => post.get({ plain: true }));
+			const posts = dbPostData.map((post) => post.get({ plain: true }));
 			// pass a single post object into the homepage template
 			res.render("homepage", { posts });
 		})
@@ -40,6 +41,16 @@ router.get("/", (req, res) => {
 			console.log(err);
 			res.status(500).json(err);
 		});
+});
+
+router.get("/login", (req, res) => {
+	res.render("login");
+	if (req.session.loggedIn) {
+		res.redirect("/");
+		return;
+	}
+
+	res.render("login");
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,19 @@ const express = require("express");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const path = require('path');
+const session = require('express-session');
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUnintialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -16,7 +28,7 @@ app.set('view engine', 'handlebars')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(session(sess));
 // turn on routes
 app.use(routes);
 
